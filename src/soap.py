e9f2e656d6f057b5f4354a0dc04e934186ca0017
Juan773 <juanquezada@upeu.edu.pe>
2020-05-21 16:18:57 -0500

    soap

--- src/soap.py
+++ src/soap.py
@@ -1,7 +1,7 @@
 from zeep import Client
 
 cliente = Client(wsdl='http://www.dneonline.com/calculator.asmx?WSDL')
-print(cliente.service.Multiply(4,10))
-print(cliente.service.Add(4,10))
-print(cliente.service.Divide(4,10))
-print(cliente.service.Subtract(4,10))
+print(cliente.service.Multiply(2,10))
+print(cliente.service.Add(2,10))
+print(cliente.service.Divide(10,2))
+print(cliente.service.Subtract(10,2))
---
+++ src/soap3.py
@@ -0,0 +1,25 @@
+from zeep import Client
+
+cliente = Client(wsdl='https://cvnet.cpd.ua.es/servicioweb/publicos/pub_gestdocente.asmx?wsdl')
+print(cliente.service.wsagrupaciones {
+        'plengua:':'Tormenta',
+        'pcurso:':'Nublado',
+        'pcoddep:': 'Despejado'
+  
+    })
+
+#print(cliente.service.wsareasdpto (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsasidepto (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsasiplan (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wscalificaciones (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsconvemovilidad (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsconvocatorias (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsdatosasig (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsdatosplan (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsdptoasig (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsdptocen (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsfechaexamenesasi (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsfechaexamenesasinue (["asdasd","asdasd","asd","asdad"]))
+#print(cliente.service.wsfechaexamenesasitotal (["asdasd","asdasd","asd","asdad"]))
+
+|
